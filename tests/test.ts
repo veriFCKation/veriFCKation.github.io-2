b8be25d900ccaadcb005196541f6a0a6c7fefd9f
veriFCKation <99489584+veriFCKation@users.noreply.github.com>
2022-07-11 23:34:43 +0300

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 test('test fetching', async ({ page }) => {
   await page.goto('/');
   await page.locator('#pictureDog').click();
-  expect(await page.textContent('#dogImg')).not.toBeUndefined();
+  expect(await page.textContent('#dogImg')).not.toBeNull();
 });
