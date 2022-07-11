ee40b20fd0bb52ef14898ef946150099babaf3bb
veriFCKation <k.shchekina@innopolis.university>
2022-07-11 23:05:09 +0300

    up

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +9,17 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
+		prerender: {
+			default: true,
+		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		}
+		},
+		// paths: {
+		// 	base: dev ? '' : '/repository.github.io',
+		// },
+		trailingSlash: 'always'
 	}
 };
 
