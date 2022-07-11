30a81e8ece1ec2bd1594825d5d5e94211dd0d20e
veriFCKation <k.shchekina@innopolis.university>
2022-07-11 23:24:57 +0300

    add playwright

---
+++ tests/test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+
+test('test fetching', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#pictureDog').click();
+  expect(await page.textContent('#dogImg')).not.toBeUndefined();
+});
